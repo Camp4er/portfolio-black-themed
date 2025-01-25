cfb616fd7f4ae3d4a7c87542bc6429b515c8d921
Camp4er <camp4ercode@gmail.com>
2025-01-26 02:30:06 +0530

    scroll and click fix try

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section className="min-h-screen flex flex-col items-center justify-center">
     <h2 className="text-4xl font-bold mb-6">Contact</h2>
-    <p className="text-lg">Reach out via email: <a href="mailto:saxenapoorva2004@gmail.com" className="underline">saxenapoorva2004@gmail.com</a></p>
+    <p className="text-lg">Reach out via email: <a href="mailto:saxenapoorva2004@gmail.com" className="underline" target='_blank'>saxenapoorva2004@gmail.com</a></p>
   </section>
   )
 }
